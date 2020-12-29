8932f70b99b1263a7cb24633f0bd5b6c1bb89433
ChristyTropila <tropila04@gmail.com>
2020-12-28 21:04:01 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react'
+import Login from './Login.js'
+
+function App() {
+  return (
+   <div className="app">
+     <Login/>
+   </div>
+  );
+}
+
+export default App;
